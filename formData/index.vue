17b29b42f2599b3456b66fb4c0108b6ae14f1edf
charmingsong <web_songsong@163.com>
2020-12-31 20:04:53 +0800

    feat: add formData template

---
+++ formData/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="{{fileNmae}}">
+    这是用户定义的filename : {{fileNmae}}
+    这是组件内部的数据: \{{message}}
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      message: 'this template message'
+    }
+  },
+ }
+</script>
+<style lang="less" scoped>
+.{{fileNmae}}{
+}
+</style>
