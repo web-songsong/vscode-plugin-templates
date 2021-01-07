825bde2c4307fa3f46e0c238fef96f77559bd47d
charmingsong <web_songsong@163.com>
2021-01-07 11:06:12 +0800

    feat: test

---
+++ demo1-写入变量的文件格式/test1.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="{{fileName}}">
+    .vue文件 可以获取写入变量fileName: {{fileName}}
+    模板中自定义变量 {{{message}}}
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      message: 'hellow word'
+    }
+  },
+}
+</script>
+<style lang="less" scoped>
+.{{fileName}} {
+}
+</style>
